406508a0bfc463d3db5adb014c07128c558fe905
hyunsung213 <hyunsung213312@gmail.com>
2026-04-19 23:52:49 +0900

    please

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api",
 });
 
 apiClient.interceptors.request.use((config) => {
